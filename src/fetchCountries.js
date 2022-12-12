d821fc70f4936b94fdae372925d4bbeaf442e78b
olenaTeplodar <lena.golubenkova@gmail.com>
2022-12-12 16:47:49 +0200

    add styles

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+const URL = 'https://restcountries.com/v3.1/name/';
+
+const FIELDS = '?fields=name,capital,population,flags,languages';
+
+export const fetchCountries = country => {
+  return fetch(URL + country + FIELDS).then(result => {
+    if (!result.ok) throw new Error(`HTTP error: ${result.status}`);
+    return result.json();
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+Notiflix.Notify.init({
+    timeout: 5000,
+});
 
 const DEBOUNCE_DELAY = 300;
