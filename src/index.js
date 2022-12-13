9847f64d0d4bcf05503a6fad3920479689095ef9
olenaTeplodar <lena.golubenkova@gmail.com>
2022-12-13 13:43:05 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,87 @@ Notiflix.Notify.init({
 });
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    searchField: document.querySelector('#search-box'),
+    countryListEl: document.querySelector('.country-list'),
+    countryInfoEl: document.querySelector('.country-info'),
+};
+
+refs.searchField.addEventListener(
+    'input',
+    debounce(OnSearchBoxInput, DEBOUNCE_DELAY)
+);
+
+function OnSearchBoxInput(event) {
+    event.preventDefault();
+    clearCountryMarkup();
+    const searchField = refs.searchField.value.trim();
+    if (!searchField) return;
+
+    fetchCountries(searchField)
+    .then(countries => {
+        if (countries.length > 10) {
+        Notiflix.Notify.info(
+            `Too many matches found. Please enter a more specific name.`
+        );
+        } else if (countries.length >= 2) {
+        appendCountriesMarkup(countries);
+        } else {
+        appendCountryMarkup(countries);
+        }
+    })
+    .catch(err => {
+        Notiflix.Notify.failure(`Oops, there is no country with that name`);
+    });
+}
+
+function appendCountriesMarkup(countries) {
+    clearCountryMarkup();
+    const markup = countries
+    .map(country => {
+        const {
+        flags: { svg },
+        name: { common: commonName },
+        } = country;
+
+        return `
+            <li class ='country-list-element'>
+            <img src='${svg}' alt='flag of ${commonName}' width='50' height='25'>
+            <p>${commonName}</p>
+            </li>
+        `;
+    })
+    .join('');
+    refs.countryListEl.innerHTML = markup;
+}
+
+function appendCountryMarkup(countries) {
+    clearCountryMarkup();
+    const markup = countries
+    .map(country => {
+        const {
+        capital,
+        population,
+        name: { common: commonName },
+        flags: { svg },
+        languages,
+        } = country;
+
+        const valuesLanguages = Object.values(languages).join(', ');
+
+        return `
+            <h2 class ='country-list-element'><img src='${svg}' alt='flag of ${commonName}' width='75' height='50'>
+            <p>${commonName}</p></h2>
+            <p><strong>Capital:</strong> ${capital}</p>
+            <p><strong>Population:</strong> ${population}</>
+            <p><strong>Languages:</strong> ${valuesLanguages}</>
+        `;
+    })
+    .join('');
+    refs.countryInfoEl.innerHTML = markup;
+}
+
+function clearCountryMarkup() {
+    refs.countryListEl.innerHTML = '';
+    refs.countryInfoEl.innerHTML = '';
+}
